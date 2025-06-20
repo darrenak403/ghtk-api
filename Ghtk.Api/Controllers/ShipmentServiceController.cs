52f8f1671d0fa9a871bc54f432c7009d2ae19e40
ngothanhdat-AK <ngothanhdat4002@gmail.com>
2025-06-21 04:13:03 +0700

    Add project files.

---
+++ Ghtk.Api/Controllers/ShipmentServiceController.cs
@@ -0,0 +1,21 @@
+﻿using Ghtk.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ghtk.Api.Controllers
+{
+    [Route("/services/shipment/")]
+    [ApiController]
+    public class ShipmentServiceController : ControllerBase
+    {
+        public ShipmentServiceController()
+        {
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder([FromBody] CreateOrder shipment)
+        {
+            return Ok();
+        }
+    }
+}
